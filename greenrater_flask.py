6a14aef8576dfcb1f522eb9b33e9b59110a873e4
lurplee <168366442+lurplee@users.noreply.github.com>
2026-05-07 11:16:58 -0400

    Use environment variable for SECRET_KEY

--- greenrater_flask.py
+++ greenrater_flask.py
@@ -10,7 +10,7 @@ from flask import request, render_template, redirect, url_for, session
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-        SECRET_KEY='dev',
+        SECRET_KEY=os.environ.get('SECRET_KEY', 'dev'),
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
     )
 
